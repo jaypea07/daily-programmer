8ed5089460b2179bbcb1de887d63330ad1083738
jaypea07 <jaypea07@gmail.com>
2016-12-23 15:52:06 -0600

    Formatting
    Moving around one of the console logs and converting from tabs to
    spaces.

--- 295_easy_letter-by-letter/index.js
+++ 295_easy_letter-by-letter/index.js
@@ -1,15 +1,15 @@
 const update = (source, target) => {
-	console.log(source);
+  let targetArray = target.split('');
+  console.log(source);
 
-	let targetArray = target.split('');
+  for (var i = 0; i < source.length; i++) {
+    sourceArray = source.split('');
 
-	for (var i = 0; i < source.length; i++) {
-		sourceArray = source.split('');
-		
-		if (sourceArray[i] !== targetArray[i]) {
-			sourceArray[i] = targetArray[i];
-			source = sourceArray.join('');
-			console.log(source);
-		}
-	}
+    if (sourceArray[i] !== targetArray[i]) {
+      sourceArray[i] = targetArray[i];
+      source = sourceArray.join('');
+      console.log(source);
+    }
+
+  }
 };
