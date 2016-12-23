87a6cc82cba8454d08423f1bd407034f950dd383
jaypea07 <jaypea07@gmail.com>
2016-12-23 17:11:19 -0600

    Adding the 2016-12-22 challenge
    Adding the first pass - there’s still lots of cleanup to do

---
+++ 296_intermediate_overlapping-rectangles/README.md
@@ -0,0 +1,3 @@
+# [2016-12-22] Challenge #296 [Intermediate] Intersecting Area Of Overlapping Rectangles
+
+**URL:** https://www.reddit.com/r/dailyprogrammer/comments/5jpt8v/20161222_challenge_296_intermediate_intersecting/
---
+++ 296_intermediate_overlapping-rectangles/index.js
@@ -0,0 +1,43 @@
+const findOverlapingArea = (firstRec, secondRec) => {
+  firstRec = getCoordinates(firstRec);
+  secondRec = getCoordinates(secondRec);
+
+  let xOverlaps = (firstRec[0].x <= secondRec[0].x && secondRec[0].x <= firstRec[1].x) || (firstRec[0].x >= secondRec[0].x && secondRec[0].x >= firstRec[1].x);
+  let yOverlaps = (firstRec[0].y <= secondRec[0].y && secondRec[0].y <= firstRec[1].y) || (firstRec[0].y >= secondRec[0].y && secondRec[0].y >= firstRec[1].y);
+  let xLength;
+  let yLength;
+
+  if (xOverlaps) {
+    let xValues = [firstRec[0].x, firstRec[1].x, secondRec[0].x, secondRec[1].x].map((x) => {
+      return parseFloat(x);
+    }).sort((a, b) => {
+      return a - b;
+    });
+    xLength = xValues[2] - xValues[1];
+  } else {
+    xLength = 0;
+  }
+
+  if (yOverlaps) {
+    let yValues = [firstRec[0].y, firstRec[1].y, secondRec[0].y, secondRec[1].y].map((y) => {
+      return parseFloat(y);
+    }).sort((a, b) => {
+      return a - b;
+    });
+    yLength = yValues[2] - yValues[1];
+  } else {
+    yLength = 0;
+  }
+
+  console.log(xLength * yLength);
+};
+
+const getCoordinates = (coordinateString) => {
+  return coordinateString.split(' ').map((coord) => {
+    let seperator = coord.indexOf(',');
+    return {
+      x: coord.slice(0, seperator),
+      y: coord.slice(seperator + 1)
+    };
+  });
+};
