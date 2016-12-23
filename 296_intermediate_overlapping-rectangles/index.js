5a5e978a482356350e4fc1b6bbd4db8865d250f8
jaypea07 <jaypea07@gmail.com>
2016-12-23 17:41:28 -0600

    Drying up the if's

--- 296_intermediate_overlapping-rectangles/index.js
+++ 296_intermediate_overlapping-rectangles/index.js
@@ -1,20 +1,20 @@
 const findOverlapingArea = (firstRec, secondRec) => {
   firstRec = getCoordinates(firstRec);
   secondRec = getCoordinates(secondRec);
-  let xLength;
-  let yLength;
+  let xLength = getLength(firstRec[0].x, firstRec[1].x, secondRec[0].x, secondRec[1].x);
+  let yLength = getLength(firstRec[0].y, firstRec[1].y, secondRec[0].y, secondRec[1].y);
 
-  if (overlapping(firstRec[0].x, secondRec[0].x, firstRec[1].x)) {
-    xLength = getLength(firstRec[0].x, firstRec[1].x, secondRec[0].x, secondRec[1].x);
-  } else {
-    xLength = 0;
-  }
+  // if (overlapping(firstRec[0].x, secondRec[0].x, firstRec[1].x)) {
+  //   xLength = 
+  // } else {
+  //   xLength = 0;
+  // }
 
-  if (overlapping(firstRec[0].y, secondRec[0].y, firstRec[1].y)) {
-    yLength = getLength(firstRec[0].y, firstRec[1].y, secondRec[0].y, secondRec[1].y);
-  } else {
-    yLength = 0;
-  }
+  // if (overlapping(firstRec[0].y, secondRec[0].y, firstRec[1].y)) {
+  //   yLength
+  // } else {
+  //   yLength = 0;
+  // }
 
   console.log(xLength * yLength);
 };
@@ -34,10 +34,14 @@ const overlapping = (a, b, c) => {
 };
 
 const getLength = (a, b, c, d) => {
-  let ar = [a, b, c, d].map((n) => {
-    return parseFloat(n);
-  }).sort((a, b) => {
-    return a - b;
-  });
-  return ar[2] - ar[1];
+  if (overlapping(a, c, b)) {
+    let ar = [a, b, c, d].map((n) => {
+      return parseFloat(n);
+    }).sort((a, b) => {
+      return a - b;
+    });
+    return ar[2] - ar[1];
+  } else {
+    return 0;
+  }
 };
