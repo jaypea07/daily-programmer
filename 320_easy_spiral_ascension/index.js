d1b6ab8a8bae59219a9879245e525c2ce5053559
jaypea07 <jaypea07@gmail.com>
2017-06-24 12:24:23 -0500

    Making a dynamic spiral

--- 320_easy_spiral_ascension/index.js
+++ 320_easy_spiral_ascension/index.js
@@ -11,53 +11,59 @@ const spiral = input => {
   }
   count++;
 
-  // Right row
-  for (let i = 1; i < input; i++) {
-    matrix[i][input - 1] = count;
-    count++;
-  }
+  // console.log(Math.floor(input / 2));
 
-  // Bottom row
-  for (let i = (input - 2); i >= 0; i--) {
-    matrix[input - 1][i] = count;
-    count++;
-  }
+  for (let k = 0; k < (Math.floor(input / 2)); k++) {
+    // Right row
+    for (let i = k + 1; i < (input - k); i++) {
+      // console.log(i, count);
+      matrix[i][input - k - 1] = count;
+      count++;
+    }
 
-  // Left row
-  for (let i = (input - 2); i > 0; i--) {
-    matrix[i][0] = count;
-    count++;
-  }
+    // Bottom row
+    for (let i = (input - (k*2) - 1); i > 0; i--) {
+      // console.log(i, count);
+      matrix[input - k - 1][i-1+k] = count;
+      count++;
+    }
 
-  // Top row
-  for (let i = 1; i < (input - 1); i++) {
-    matrix[1][i] = count;
-    count++;
+    // Left row
+    for (let i = (input - k - 2); i > k; i--) {
+      matrix[i][k] = count;
+      count++;
+    }
+
+    // Top row
+    for (let i = k + 1; i < (input - k - 1); i++) {
+      matrix[k + 1][i] = count;
+      count++;
+    }
   }
 
   // Right row again
-  for (let i = 2; i < (input - 1); i++) {
-    matrix[i][input - 2] = count;
-    count++;
-  }
+  // for (let i = 2; i < (input - 1); i++) {
+  //   matrix[i][input - 2] = count;
+  //   count++;
+  // }
 
   // Bottom row again
-  for (let i = (input - 3); i >= 1; i--) {
-    matrix[input - 2][i] = count;
-    count++;
-  }
+  // for (let i = (input - 3); i >= 1; i--) {
+  //   matrix[input - 2][i] = count;
+  //   count++;
+  // }
 
   // Left row again
-  for (let i = (input - 3); i > 1; i--) {
-    matrix[i][1] = count;
-    count++;
-  }
+  // for (let i = (input - 3); i > 1; i--) {
+  //   matrix[i][1] = count;
+  //   count++;
+  // }
 
   // Top row again
-  for (let i = 2; i < (input - 2); i++) {
-    matrix[2][i] = count;
-    count++;
-  }
+  // for (let i = 2; i < (input - 2); i++) {
+  //   matrix[2][i] = count;
+  //   count++;
+  // } 
 
   console.log(matrix);
 };
