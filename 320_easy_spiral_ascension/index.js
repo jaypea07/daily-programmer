bb3858d76205514015fb2910c13fbb68e858586e
jaypea07 <jaypea07@gmail.com>
2017-06-24 12:29:05 -0500

    Cleaning up

--- 320_easy_spiral_ascension/index.js
+++ 320_easy_spiral_ascension/index.js
@@ -1,38 +1,36 @@
 const spiral = input => {
-  const lastNumber = Math.pow(input, 2);
-  const loops = Math.ceil(input / 2);
   let matrix = [];
   let count;
 
-  // Top row
+  // First row
   for (count = 0; count < input; count++) {
     matrix.push([]);
     matrix[0].push(count + 1);
   }
   count++;
 
-  for (let k = 0; k < (Math.floor(input / 2)); k++) {
+  for (let loop = 0; loop < (Math.floor(input / 2)); loop++) {
     // Right row
-    for (let i = k + 1; i < (input - k); i++) {
-      matrix[i][input - k - 1] = count;
+    for (let i = loop + 1; i < (input - loop); i++) {
+      matrix[i][input - loop - 1] = count;
       count++;
     }
 
     // Bottom row
-    for (let i = (input - (k*2) - 1); i > 0; i--) {
-      matrix[input - k - 1][i-1+k] = count;
+    for (let i = (input - (loop * 2) - 1); i > 0; i--) {
+      matrix[input - loop - 1][loop + i - 1] = count;
       count++;
     }
 
     // Left row
-    for (let i = (input - k - 2); i > k; i--) {
-      matrix[i][k] = count;
+    for (let i = (input - loop - 2); i > loop; i--) {
+      matrix[i][loop] = count;
       count++;
     }
 
     // Top row
-    for (let i = k + 1; i < (input - k - 1); i++) {
-      matrix[k + 1][i] = count;
+    for (let i = loop + 1; i < (input - loop - 1); i++) {
+      matrix[loop + 1][i] = count;
       count++;
     }
   }
